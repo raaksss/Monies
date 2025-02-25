f61710c49c50d75b35f1277b24c1f0b3b2071a17
raaksss <rakshitamanocha@gmail.com>
2025-02-25 17:56:26 +0530

    Backend setup and landing page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="mt-40">
+      <HeroSection />
     </div>
   );
 }
