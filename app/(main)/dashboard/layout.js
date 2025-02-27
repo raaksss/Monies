9be813b87ae9f48ce9c7ab89bcc35513a4fc8f96
raaksss <rakshitamanocha@gmail.com>
2025-02-27 19:57:14 +0530

    Create account form UI

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import DashboardPage from './page'
+import {BeatLoader} from 'react-spinners'
 const DashboardLayout = () => {
   return (
     <div className='px-5'>
        <h1 className='text-6xl font-bold gradient-title mb-5'>Dashboard</h1>
 
+<Suspense fallback={<BeatLoader className='mt-4' width={"100%"} color="#9333ea"/>}>
        <DashboardPage />
+       </Suspense>
     </div>
   )
 }
