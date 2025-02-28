cf8abd68539dd2253b55302a7c9c3791b8934298
raaksss <rakshitamanocha@gmail.com>
2025-02-28 17:30:46 +0530

    Added dummy transactions using seed

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -2,8 +2,9 @@ import { getAccountWithTransactions } from '@/actions/accounts'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
-const AccountsPage = async ({ params: { id } }) => {
+const AccountsPage = async ({ params}) => {
 
+    const {id}= await params
     const accountData = await getAccountWithTransactions(id);
     
     if(!accountData){
@@ -13,27 +14,28 @@ const AccountsPage = async ({ params: { id } }) => {
     const{transations, ...account}= accountData;
 
   return (
-    <div className="space-y-8 px-5">
-    <div className="flex gap-4 items-end justify-between">
-      <div>
-        <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
-          {account.name}
-        </h1>
-        <p className="text-muted-foreground">
-          {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
-          Account
-        </p>
-      </div>
-
-      <div className="text-right pb-2">
-        <div className="text-xl sm:text-2xl font-bold">
-          ${parseFloat(account.balance).toFixed(2)}
+     <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+            {account.name}
+          </h1>
+          <p className="text-muted-foreground">
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
+            Account
+          </p>
         </div>
-        <p className="text-sm text-muted-foreground">
-          {account._count.transactions} Transactions
-        </p>
+
+        <div className="text-right pb-2">
+          <div className="text-xl sm:text-2xl font-bold">
+            ${parseFloat(account.balance).toFixed(2)}
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {account._count.transactions} Transactions
+          </p>
+        </div>
       </div>
-    </div>
+   
     </div>
   )
 }
