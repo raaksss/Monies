d8995545c609f9771149a5086bf6b7e85f84483c
raaksss <rakshitamanocha@gmail.com>
2025-02-28 14:10:08 +0530

    Add Accounts ui and handle default toggle client+server

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -6,15 +6,15 @@ import { db } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
 const serializeTransaction = (obj) => {
-    const serialized = { ...obj };
-    if (obj.balance) {
-      serialized.balance = obj.balance.toNumber();
-    }
-    if (obj.amount) {
-      serialized.amount = obj.amount.toNumber();
-    }
-    return serialized;
-  };
+  const serialized = { ...obj };
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+  if (obj.amount) {
+    serialized.amount = obj.amount.toNumber();
+  }
+  return serialized;
+};
 
 
 export async function createAccount(data){
@@ -64,4 +64,37 @@ export async function createAccount(data){
     } catch (error) {
         console.log(error.message)
     }
+}
+
+export async function getUserAccounts() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  try {
+    const accounts = await db.account.findMany({
+      where: { userId: user.id },
+      orderBy: { createdAt: "desc" },
+      include: {
+        _count: {
+          select: {
+            transactions: true,
+          },
+        },
+      },
+    });
+
+    const serializedAccounts = accounts.map(serializeTransaction);
+
+    return serializedAccounts;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import CreateAccountDrawer from '@/components/create-account-drawer'
 import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
+import { getUserAccounts } from '@/actions/dashboard'
+import {AccountCard} from './_components/account-card'
 
-const DashboardPage = () => {
+
+async function DashboardPage() {
+  const accounts=await getUserAccounts()
   return (
     <div className='px-5'>
       <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3 '>
@@ -15,6 +19,12 @@ const DashboardPage = () => {
           </CardContent>
         </Card>
         </CreateAccountDrawer>
+
+        {accounts.length > 0 &&
+          accounts?.map((account) => (
+            <AccountCard key={account.id} account={account} />
+          ))}
+
         </div>
     
     </div>
