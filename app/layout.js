a2065e800075b6662e5b186e92e0360d93e03417
raaksss <rakshitamanocha@gmail.com>
2025-03-08 22:04:27 +0530

    Footer padding change

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,8 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
 
-            <footer className="bg-blue-50 dark:bg-gray-900 py-12">
+           
+            <footer className="bg-blue-50 dark:bg-gray-900 py-6">
               <div className="container mx-auto px-4 text-center text-gray-600 dark:text-gray-400">
                 <p>Track every penny.</p>
               </div>
